52169224a18de5f3ec55f64e7761fbb2f6a2f33d
kazi <60290379+kazipex@users.noreply.github.com>
2025-09-09 13:19:54 +0100

    Adding current files to repo

---
+++ index.js
@@ -0,0 +1,6 @@
+function buttonDown(){
+    document.getElementById("button1").style.backgroundColor = "rgba(219, 219, 219, 1)";
+}
+function buttonUp(){
+    document.getElementById("button1").style.backgroundColor = "rgb(243, 243, 243)";
+}
